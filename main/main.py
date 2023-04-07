36c1c36f6252f07aae31621c3fc44201ec2051f8
Juan Serna <jserna7856@gmail.com>
2023-04-07 16:25:36 -0500

    Stock_algo menu

--- main/main.py
+++ main/main.py
@@ -4,6 +4,9 @@ import config
 import configEx
 ORDER_URL = '{}/v2/orders'. format(config.ALPACA_API_BASE_URL)
 
+
+
+
 def create_market_order():
     ticker = 'AAPL'
     qty = '2'
@@ -43,8 +46,25 @@ def create_limit_order():
     r = requests.post(ORDER_URL, json = data, headers = configEx.HEADERS)
     return r.content
 
+def stock_menu(n):
+    if(n == 1):
+        print("Creating market order.....")
+        create_market_order()
+    elif(n == 2):
+        print("Creating limit order....")
+        create_limit_order()
+    elif(n == 0):
+        print("Return to menu")
+        stock_menu(n)
+
+
 
 
 if __name__ == "__main__":
+    n = int(input("Welcome to Stock_Algo \n Select an option: \n 1.Create market order \n 2.Create limit order  \n Select option: " ))
+    stock_menu(n)
+    
+    """
     print(create_market_order())
     print(create_limit_order())
+    """
