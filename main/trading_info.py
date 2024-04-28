42abf2098f62333fd367c216ee09f17e12349832
Juan Serna <103015719+NoT-Serna@users.noreply.github.com>
2024-04-27 22:47:22 -0500

    Update trading_info.py

--- main/trading_info.py
+++ main/trading_info.py
@@ -1,5 +1,4 @@
-from tradingview_ta import TA_Handler, Interval, Exchange
-
+from tradingview_ta import TA_Handler, Interval
 
 class Screener():
 
@@ -7,33 +6,39 @@ class Screener():
         self.symbol = symbol
         self.screener = screener
         self.exchange = exchange
-        self.interval = Interval
-        self.data = ()
-
+        self.interval = None
+        self.data = None
     
-
     def detailed_search(self):
-        if "1m" in self.interval:
+        interval_mapping = {
+            "1m": Interval.INTERVAL_1_MINUTE,
+            "5m": Interval.INTERVAL_5_MINUTES,
+            "15m": Interval.INTERVAL_15_MINUTES,
+            "30m": Interval.INTERVAL_30_MINUTES,
+            "1h": Interval.INTERVAL_1_HOUR,
+            "2h": Interval.INTERVAL_2_HOURS,
+            "4h": Interval.INTERVAL_4_HOURS,
+            "1d": Interval.INTERVAL_1_DAY,
+            "1W": Interval.INTERVAL_1_WEEK,
+            "1M": Interval.INTERVAL_1_MONTH
+        }
+
+        interval_constant = interval_mapping.get(self.interval)
+        if interval_constant:
             self.data = TA_Handler(
-                symbol= self.symbol,
-                screener = self.screener,
-                exchange = self.exchange,
-                interval = Interval.INTERVAL_1_MINUTE
+                symbol=self.symbol,
+                screener=self.screener,
+                exchange=self.exchange,
+                interval=interval_constant
             )
 
-            return self.data
-        
-        elif "5m" in self.interval:
-            self.data = TA_Handler(
-                symbol = self.symbol,
-                screener= self.screener,
-                exchange = self.exchange,
-                interval = Interval.INTERVAL_5_MINUTES
-            )
-
-            return self.data
+            if self.data:
+                return self.data.get_analysis().summary
+            else:
+                return "No data available"
+        else:
+            return "Invalid interval specified"
     
-
     def search(self):
         return f'''
         SUMMARY:
@@ -43,41 +48,24 @@ class Screener():
         Interval = {self.interval}
         '''
 
-            
-    
     def show_search(self):
-        r = self.data.get_analysis().summary
-
+        if self.data:
+            return self.data.get_analysis().summary
+        else:
+            return "No data available"
 
 def main():
     symbol = input("Choose the ticker for analysis: ")
-    screener = input("Choose where the asset is being screened (america,crypto etc..):  ")
-    exchange = input("Market of circulation: " )
-    interval = input("Time of analysis: ")
+    screener = input("Choose where the asset is being screened (america, crypto, etc.): ")
+    exchange = input("Market of circulation: ")
+    interval = input("Time of analysis (1m, 5m, 15m, 30m, 1h, 2h, 4h, 1d, 1W, 1M): ")
 
-    asset = Screener(symbol,screener,exchange)
+    asset = Screener(symbol, screener, exchange)
+    asset.interval = interval
     asset.detailed_search()
     
     print(asset.search())
     print(asset.show_search())
-
-
-
-
-
-
-'''
-def run():
-    tesla = TA_Handler(
-        symbol = "TSLA",
-        screener= "america",
-        exchange= "NASDAQ",
-        interval = Interval.INTERVAL_5_MINUTES
-    )
-    print(tesla.get_analysis().summary)
-'''
-
-
 
 if __name__ == "__main__":
    main()
